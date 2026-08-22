90dc52a19c5e3193710e6a2289e89af51281f324
rubenzfox <rtornero@gmail.com>
2026-08-21 22:37:57 -0500

    Agrego conexión a MariaDB y pruebas3

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ app.use(express.json());
 
 // Configuración de MariaDB
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST || 'serverless-us-west1.sysp0000.db2.skysql.com',
-  port: process.env.DB_PORT || 4107,
-  user: process.env.DB_USER || 'dbpgf31924605',
-  password: process.env.DB_PASSWORD || '[iDI1nJe6lHt87SzwHi+lBdP9',
-  database: process.env.DB_NAME || 'mysql',
+  //host: process.env.DB_HOST || 'serverless-us-west1.sysp0000.db2.skysql.com',
+  //port: process.env.DB_PORT || 4107,
+  //user: process.env.DB_USER || 'dbpgf31924605',
+  //password: process.env.DB_PASSWORD || '[iDI1nJe6lHt87SzwHi+lBdP9',
+  //database: process.env.DB_NAME || 'mysql',
+
+  host: 'serverless-us-west1.sysp0000.db2.skysql.com',
+  port: 4107,
+  user: 'dbpgf31924605',
+  password:  '[iDI1nJe6lHt87SzwHi+lBdP9',
+  database:  'mysql',
   connectionLimit: 5,
   connectTimeout: 10000,   // 10 segundos para establecer la conexión
   acquireTimeout: 10000    // 10 segundos para obtener una conexión del pool
